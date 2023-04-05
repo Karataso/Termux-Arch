3ff8859f3b4b8d00574b2e619da35ff3e05f6ac1
Karataso <111629664+Karataso@users.noreply.github.com>
2023-04-05 12:29:09 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+pkg update && pkg upgrade
+pkg install python
+pip install termcolor
+pkg install proot-distro
+cp st.py ~
+mv bashrc.txt .bashrc
+cp .bashrc ~
+proot-distro install archlinux
