dc548f46f3faa28cc4345464e7f69c2f86d1dc51
Karataso <111629664+Karataso@users.noreply.github.com>
2023-04-05 14:00:12 -0600

    Create ex.py

---
+++ ex.py
@@ -0,0 +1,8 @@
+from termcolor import colored, cprint
+cprint("´TermuxArch by Karataso´", "green")
+cprint("--------------------------------------------", "cyan")
+cprint("Instalacion finished", "cyan")
+cprint("you may reboot Termux now", "cyan")
+cprint("Type exit below, or close the app entirely (Even the notification)", "cyan")
+cprint("after reboot you will automaticaly enter arch", "cyan")
+cprint("-----------------------------", "cyan")
