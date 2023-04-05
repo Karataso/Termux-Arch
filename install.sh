42522dc2fe3ca268b651af7e1456a5c3c65fd451
Karataso <111629664+Karataso@users.noreply.github.com>
2023-04-05 13:49:43 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 pkg update && pkg upgrade
 pkg install python
 pip install termcolor
+pkg install zsh
+chsh -s /zsh
 pkg install proot-distro
 cp st.py ~
-mv bashrc .bashrc
+mv zshrc .zshrc
 cp .bashrc ~
 proot-distro install archlinux
