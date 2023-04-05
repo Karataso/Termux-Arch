6d681cbed514291e0023e4e4cad6833efa000b06
Karataso <111629664+Karataso@users.noreply.github.com>
2023-04-05 13:03:24 -0600

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pkg update && pkg upgrade
-pkg install python
-pip install termcolor
-pkg install proot-distro
-cp st.py ~
-mv bashrc.txt .bashrc
-cp .bashrc ~
-proot-distro install archlinux
