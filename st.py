ed7a819a513823efdf4a993f01331b9bfcc127d1
Karataso <111629664+Karataso@users.noreply.github.com>
2023-04-06 16:05:07 -0600

    Update st.py

--- st.py
+++ st.py
@@ -1,10 +1,10 @@
 from termcolor import cprint, colored
 #asignamos todas las bariables, con color para poder dibujarlas despues
-cprint("          / \", "cyan")
-cprint("         /   \", "cyan")  
+cprint("          / \ ", "cyan")
+cprint("         /   \ ", "cyan")  
 cprint("        /^.   \ ", "cyan")
 cprint("       /  .-.  \ ", "cyan")
 cprint("      /  (   ) _\ ", "cyan")
-cprint("     / _.~   ~._^\", "cyan")
+cprint("     / _.~   ~._^\ ", "cyan")
 cprint("    /.^         ^.\ ", "cyan")
 cprint("Welcome to Termux-Arch by Karataso", "cyan")
