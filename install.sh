bd32660db0dd4a47434ac56d14775a3a5d8777f1
Karataso <111629664+Karataso@users.noreply.github.com>
2023-04-06 09:46:16 -0700

    Update install.sh
    made the installer more friendly

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 pkg update && pkg upgrade
+clear
 pkg install python
+clear
 pip install termcolor
 clear
-python question.py
 pkg install zsh
 chsh -s /zsh
 pkg install proot-distro
+clear
 cp st.py ~
 mv zshrc .zshrc
 cp .zshrc ~
